15df1ffc545552848920a52986d64006626dc89f
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-12-05 10:18:19 +0100

    Calculates correctly, but cant show path.

--- Zad_4.cpp
+++ Zad_4.cpp
@@ -4,7 +4,9 @@
 #include <sstream>
 #include <algorithm>
 
-const bool DEBUG = false;
+const bool DEBUG = true;
+
+#define noop() ({;});
 
 class Graph {
 
@@ -12,7 +14,7 @@ public:
     /// Constructor
     Graph();
 
-    /// Shows graph's matrix
+    /// Shows graph's distance_matrix
     void show_graph_matrix();
 
     void load_graph_matrix_from_stdin();
@@ -24,7 +26,9 @@ public:
 
 private:
     /// Matrix to be searched
-    int **matrix;
+    int **distance_matrix;
+    /// Parents matrix
+    int **parents_matrix;
     /// How many edges has already been loaded
     int vertex_count;
     int number_of_edges;
@@ -36,11 +40,16 @@ private:
     void print_target_paths(int *parent, int j);
 
     int minDistance(const int *dist, const bool *sptSet);
+
+    bool FloydWarshall();
+
+    void FWPath(int i, int j);
 };
 
 
 void Graph::add_edge(int vertex_u, int vertex_v, int weight) {
-    matrix[vertex_u][vertex_v] = weight;
+    distance_matrix[vertex_u][vertex_v] = weight;
+    parents_matrix[vertex_u][vertex_v] = vertex_u;
     if (DEBUG) {
         printf("Adding edge: %d -> %d : %d\n", vertex_u, vertex_v, weight);
     }
@@ -50,7 +59,14 @@ void Graph::show_graph_matrix() {
     int i, j;
     for (i = 0; i < vertex_count; i++) {
         for (j = 0; j < vertex_count; j++) {
-            printf("%*d ", 2, matrix[i][j]);
+            printf("%*d ", 2, distance_matrix[i][j] == INT_MAX ? -1 : distance_matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < vertex_count; i++) {
+        for (j = 0; j < vertex_count; j++) {
+            printf("%*d ", 2, distance_matrix[i][j] == INT_MAX ? -1 : distance_matrix[i][j]);
         }
         printf("\n");
     }
@@ -72,101 +88,84 @@ void Graph::load_graph_matrix_from_stdin() {
     }
 }
 
-int Graph::minDistance(const int dist[],
-                       const bool sptSet[]) {
-
-    // Initialize min value
-    int min = INT_MAX, min_index;
-
-    for (int v = 0; v < vertex_count; v++) {
-        if (!sptSet[v] && dist[v] <= min) {
-            min = dist[v], min_index = v;
-        }
-    }
-    return min_index;
-}
-
 void Graph::deallocate_memory() {
     for (int i = 0; i < vertex_count; ++i) {
-        delete[] matrix[i];
+        delete[] distance_matrix[i];
     }
-    delete[] matrix;
+    delete[] distance_matrix;
+}
+
+bool Graph::FloydWarshall() {
+    int i, j, k, w;
+
+    for (k = 0; k < vertex_count; k++) {
+        for (i = 0; i < vertex_count; i++) {
+            for (j = 0; j < vertex_count; j++) {
+                if ((distance_matrix[i][k] == INT_MAX) || (distance_matrix[k][j] == INT_MAX)) {
+                    continue;
+                }
+                w = distance_matrix[i][k] + distance_matrix[k][j];
+                if (distance_matrix[i][j] > w) {
+                    distance_matrix[i][j] = w;
+                    parents_matrix[i][j] = parents_matrix[k][j];
+                }
+            }
+        }
+    }
+    for (i = 0; i < vertex_count; i++) {
+        if (distance_matrix[i][i] < 0) {
+            return false;
+        } // Ujemny cykl
+    }
+    return true;
+}
+
+// Rekurencyjna procedura odtwarzania minimalnej
+// ścieżki z macierzy poprzedników p
+//----------------------------------------------
+void Graph::FWPath(int i, int j) {
+    if (i == j) {
+        printf("%d ", i);
+    } else if (parents_matrix[i][j] == -1) {
+        printf("NO PATH");
+    } else {
+        FWPath(i, parents_matrix[i][j]);
+        printf(" ");
+    }
 }
 
 void Graph::check_distances(int src_node) {
-    int dist[vertex_count];
-    bool sptSet[vertex_count];
-    int parent[vertex_count];
-    for (int i = 0; i < vertex_count; i++) {
-        if (i == src_node) {
-            parent[i] = -1;
-        } else {
-            parent[i] = INT_MAX;
-        }
-        dist[i] = INT_MAX;
-        sptSet[i] = false;
-    }
-    dist[src_node] = 0;
-    for (int count = 0; count < vertex_count - 1; count++) {
-        int u = minDistance(dist, sptSet);
-
-        // Mark the picked vertex
-        // as processed
-        sptSet[u] = true;
-
-        for (int v = 0; v < vertex_count; v++)
-
-            if (!sptSet[v] && matrix[u][v] &&
-                dist[u] + matrix[u][v] < dist[v]) {
-                parent[v] = u;
-                dist[v] = dist[u] + matrix[u][v];
+    if (FloydWarshall()) {
+        for (int j = 1; j < vertex_count; j++) {
+            printf("%d-%d: ", 0, j);
+            FWPath(0, j);
+            if (distance_matrix[0][j] < INT_MAX) {
+                printf("$%d", distance_matrix[0][j]);
             }
+        }
+        printf("\n");
     }
-    print_paths_to_other_nodes(dist, parent);
 }
 
+
 Graph::Graph() {
-    matrix = new int *[vertex_count];
-    for (int i = 0; i < vertex_count; ++i) {
-        matrix[i] = new int[vertex_count];
-        for (int j = 0; j < vertex_count; j++) {
-            matrix[i][j] = 0;
-        }
-    }
     std::string line;
     std::getline(std::cin, line);
     int space_index = line.find(' ');
     vertex_count = std::stoi(line.substr(0, space_index));
     number_of_edges = std::stoi(line.substr(space_index + 1, line.size()));
-}
-
-void Graph::print_paths_to_other_nodes(int *dist, int *parent) {
-    int src = 0;
-    for (int target_node = 1; target_node < vertex_count; target_node++) {
-        if (parent[target_node] == INT_MAX) {
-            printf("NIE ISTNIEJE DROGA Z %d DO %d\n", src+1, target_node + 1);
-            continue;
+    distance_matrix = new int *[vertex_count];
+    parents_matrix = new int *[vertex_count];
+    for (int i = 0; i < vertex_count; i++) {
+        distance_matrix[i] = new int[vertex_count];
+        parents_matrix[i] = new int[vertex_count];
+        for (int j = 0; j < vertex_count; j++) {
+            distance_matrix[i][j] = INT_MAX;
+            parents_matrix[i][j] = -1;
         }
-        printf("%d", src + 1);
-        print_target_paths(parent, target_node);
-        printf(" %d\n", dist[target_node]);
+        distance_matrix[i][i] = 0;
     }
-
-
 }
-
-void Graph::print_target_paths(int *parent, int j) {
-
-    if (parent[j] == -1) {
-        printf("Is source: %d\n", j);
-        return;
-    }
-
-    print_target_paths(parent, parent[j]);
-
-    printf("-%d", j + 1);
-};
-
 
 int main() {
     int number_of_cases;
@@ -176,10 +175,11 @@ int main() {
     for (int case_num = 0; case_num < number_of_cases; case_num++) {
         Graph graph = Graph();
         graph.load_graph_matrix_from_stdin();
+
+        graph.check_distances(0);
         if (DEBUG) {
             graph.show_graph_matrix();
         }
-        graph.check_distances(0);
         graph.deallocate_memory();
         if (case_num != number_of_cases - 1) {
             printf("\n");
